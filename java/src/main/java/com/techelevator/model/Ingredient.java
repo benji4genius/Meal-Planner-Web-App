b858318a4d18f4393cd46a8185e85915c45895ca
Jake Lilly <jacob.d.lilly@gmail.com>
2024-04-08 14:50:37 -0400

    JL: Object Models in IntelliJ

--- java/src/main/java/com/techelevator/model/Ingredient.java
+++ java/src/main/java/com/techelevator/model/Ingredient.java
@@ -1,4 +1,41 @@
 package com.techelevator.model;
 
 public class Ingredient {
+    private int ingredientId;
+
+    private String ingredientName;
+
+    private String ingredientDescription;
+
+    public Ingredient(){};
+
+    public Ingredient(int ingredientId, String ingredientName, String ingredientDescription){
+        this.ingredientId = ingredientId;
+        this.ingredientName = ingredientName;
+        this.ingredientDescription = ingredientDescription;
+    }
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public String getIngredientDescription() {
+        return ingredientDescription;
+    }
+
+    public void setIngredientDescription(String ingredientDescription) {
+        this.ingredientDescription = ingredientDescription;
+    }
 }
--- java/src/main/java/com/techelevator/model/MealPlan.java
+++ java/src/main/java/com/techelevator/model/MealPlan.java
@@ -1,4 +1,52 @@
 package com.techelevator.model;
 
+import java.util.List;
+
 public class MealPlan {
+    private int mealPlanId;
+
+    private String mealPlanName;
+
+    private String mealPlanDescription;
+
+    private List<Meal> mealList;
+
+    public MealPlan(int mealPlanId, String mealPlanName, String mealPlanDescription, List<Meal> mealList) {
+        this.mealPlanId = mealPlanId;
+        this.mealPlanName = mealPlanName;
+        this.mealPlanDescription = mealPlanDescription;
+        this.mealList = mealList;
+    }
+
+    public int getMealPlanId() {
+        return mealPlanId;
+    }
+
+    public void setMealPlanId(int mealPlanId) {
+        this.mealPlanId = mealPlanId;
+    }
+
+    public String getMealPlanName() {
+        return mealPlanName;
+    }
+
+    public void setMealPlanName(String mealPlanName) {
+        this.mealPlanName = mealPlanName;
+    }
+
+    public String getMealPlanDescription() {
+        return mealPlanDescription;
+    }
+
+    public void setMealPlanDescription(String mealPlanDescription) {
+        this.mealPlanDescription = mealPlanDescription;
+    }
+
+    public List<Meal> getMealList() {
+        return mealList;
+    }
+
+    public void setMealList(List<Meal> mealList) {
+        this.mealList = mealList;
+    }
 }
