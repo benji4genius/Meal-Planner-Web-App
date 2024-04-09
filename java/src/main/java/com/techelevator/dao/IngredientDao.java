f1c0695330e81dd1ff3d7a565c4856060bd1bac6
melissa stevens <melissastevens339@gmail.com>
2024-04-09 12:26:22 -0400

    MS: pushing igredientDAO and JDBC map

--- java/src/main/java/com/techelevator/dao/IngredientDao.java
+++ java/src/main/java/com/techelevator/dao/IngredientDao.java
@@ -1,4 +1,18 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Ingredient;
+
+import java.util.List;
+
 public interface IngredientDao {
+    public Ingredient getIngredientById(int ingredientId);
+
+    public List<Ingredient> getListOfAllIngredients();
+
+    public Ingredient addIngredient (Ingredient ingredient);
+
+    public Ingredient updateIngredient (Ingredient ingredient);
+
+    public Ingredient deleteIngredient (int ingredientID);
+
 }
