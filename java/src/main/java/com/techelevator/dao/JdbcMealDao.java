c6d6c6b927f8f1efabf92add3a5aff835740028f
Saraswati Rizal <Saraswati.rizal@gmail.com>
2024-04-09 12:08:08 -0400

    jdbcmealdao

--- java/src/main/java/com/techelevator/dao/JdbcMealDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcMealDao.java
@@ -1,4 +1,33 @@
 package com.techelevator.dao;
 
-public class JdbcMealDao {
+import com.techelevator.model.Meal;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcMealDao implements MealDao{
+
+    private JdbcTemplate jdbcTemplate;
+
+
+    private static final String SQL_SELECT_MEAL="";
+
+
+    @Override
+    public Meal getMealById(int mealId) {
+        return null;
+    }
+
+    @Override
+    public Meal getAllMeals() {
+        return null;
+    }
+
+    @Override
+    public Meal addMeal(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public Meal deleteMeal(int mealId) {
+        return null;
+    }
 }
--- java/src/main/java/com/techelevator/dao/MealDao.java
+++ java/src/main/java/com/techelevator/dao/MealDao.java
@@ -1,4 +1,21 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Meal;
+
 public interface MealDao {
+
+Meal getMealById( int mealId);
+
+
+Meal getAllMeals();
+
+Meal addMeal(Meal meal);
+
+Meal deleteMeal( int mealId);
+
+
+
+
+
+
 }
