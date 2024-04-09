24bdab20e3fd2c0e7616f294a90f9789a393b7f1
melissa stevens <melissastevens339@gmail.com>
2024-04-09 14:55:42 -0400

    MS:adding to jdbc

--- java/src/main/java/com/techelevator/dao/JdbcIngredientDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcIngredientDao.java
@@ -1,18 +1,42 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Ingredient;
+import com.techelevator.model.User;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class JdbcIngredientDao implements IngredientDao {
+    private final JdbcTemplate jdbcTemplate;
+    public jdbcIngredientDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    @Override
+    public Ingredient getIngredientById(int idIngredient) {
+        Ingredient ingredient = null;
+        String sql = "SELECT idingredient, stringredient, strdescription, strtype FROM ingredients WHERE idingredient = ?";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, idingredient);
+            if (results.next()) {
+                ingredient = mapRowToIngredient(results);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return ingredient;
+    }
 
 
     private Ingredient mapRowToIngredient(SqlRowSet result){
         Ingredient ingredient = new Ingredient();
-        ingredient.setIngredientId(result.getInt("idingredient"));
-        ingredient.setIngredientName(result.getString("stringredient"));
-        ingredient.setIngredientDescription(result.getString("strdescription"));
+        ingredient.setIdingredient(result.getInt("idingredient"));
+        ingredient.setStringredient(result.getString("stringredient"));
+        ingredient.setStrdescription(result.getString("strdescription"));
 
         return ingredient;
     }
