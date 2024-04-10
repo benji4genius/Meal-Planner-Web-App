663e811d41d948a66d611a8b0b26060d6f4c60a3
Jake Lilly <jacob.d.lilly@gmail.com>
2024-04-10 09:41:52 -0400

    JL: Login/Register styling

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -3,9 +3,11 @@
     <div id="login">
       <header>
         <nav>
-          <a href="{name: home}">Home</a>
-          <p>Meals</p>
-          <p>Meal Plans</p>
+          <a class="link" href="{name: home}">Home</a>
+          <p class="link">Link</p>
+          <img class="logo" src="Untitled Design.png"/>
+          <p class="link">Meals</p>
+          <p class="link">Meal Plans</p>
         </nav>
       </header>
       <body>
@@ -94,7 +96,7 @@ form{
 header{
   background-color:#f0754f;
   padding: 20px;
-  margin-bottom: none;
+  border: 2px solid black;
 }
 nav{
   display:flex;
@@ -113,6 +115,49 @@ nav{
   margin-left: 40%;
   padding-bottom: 45rem;
   padding-top: 10rem;
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: Neucha, sans-serif;
+}
+.logo{
+  height: 100px;
+  border: 2px solid black;
+}
+.link {
+  align-self: center;
+  background-color: #fff;
+  background-image: none;
+  background-position: 0 90%;
+  background-repeat: repeat no-repeat;
+  background-size: 4px 3px;
+  border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;
+  border-style: solid;
+  border-width: 2px;
+  box-shadow: rgba(0, 0, 0, .2) 15px 28px 25px -18px;
+  box-sizing: border-box;
+  color: #41403e;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Neucha, sans-serif;
+  font-size: 1rem;
+  line-height: 23px;
+  outline: none;
+  padding: .75rem;
+  text-decoration: none;
+  transition: all 235ms ease-in-out;
+  border-bottom-left-radius: 15px 255px;
+  border-bottom-right-radius: 225px 15px;
+  border-top-left-radius: 255px 15px;
+  border-top-right-radius: 15px 225px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.link:hover {
+  box-shadow: rgba(0, 0, 0, .3) 2px 8px 8px -5px;
+  transform: translate3d(0, 2px, 0);
+}
+
+.link:focus {
+  box-shadow: rgba(0, 0, 0, .3) 2px 8px 4px -6px;
 }
 </style>
