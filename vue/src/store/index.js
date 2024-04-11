d80619d6c538fdc013a8835a269306ea4c46a1b1
Benjamin <benji4genius@gmail.com>
2024-04-11 17:07:42 -0400

    BA: Pushing mealview

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -5,7 +5,21 @@ export function createStore(currentToken, currentUser) {
   let store = _createStore({
     state: {
       token: currentToken || '',
-      user: currentUser || {}
+      user: currentUser || {},
+
+      meals: [
+        {
+          "idmeal": 1,
+          "strmeal": "Apple Frangipan Tart",
+          "strinstructions": "Preheat the oven to 200C/180C Fan/Gas 6.\nPut the biscuits in a large re-sealable freezer bag and bash with a rolling pin into fine crumbs. Melt the butter in a small pan, then add the biscuit crumbs and stir until coated with butter. Tip into the tart tin and, using the back of a spoon, press over the base and sides of the tin to give an even layer. Chill in the fridge while you make the filling.\nCream together the butter and sugar until light and fluffy. You can do this in a food processor if you have one. Process for 2-3 minutes. Mix in the eggs, then add the ground almonds and almond extract and blend until well combined.\nPeel the apples, and cut thin slices of apple. Do this at the last minute to prevent the apple going brown. Arrange the slices over the biscuit base. Spread the frangipane filling evenly on top. Level the surface and sprinkle with the flaked almonds.\nBake for 20-25 minutes until golden-brown and set.\nRemove from the oven and leave to cool for 15 minutes. Remove the sides of the tin. An easy way to do this is to stand the tin on a can of beans and push down gently on the edges of the tin.\nTransfer the tart, with the tin base attached, to a serving plate. Serve warm with cream, crème fraiche or ice cream.",
+          "strtags": "Tart,Baking,Fruity",
+          "ingredientList": null,
+          "strmealthumb": "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
+          "stryoutube": "https://www.youtube.com/watch?v=rp8Slv4INLk"
+      }
+      ]
+
+
     },
     mutations: {
       SET_AUTH_TOKEN(state, token) {
