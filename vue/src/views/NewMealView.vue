fb524b9903a328e534e387fb084245cde4adef34
Jake Lilly <jacob.d.lilly@gmail.com>
2024-04-15 12:58:33 -0400

    New Meal Form and New Meal Form View added and given a place in the router

---
+++ vue/src/views/NewMealView.vue
@@ -0,0 +1,14 @@
+<template>
+    <new-meal />
+</template>
+
+
+
+<script>
+import NewMeal from "../components/NewMeal.vue";
+export default {
+    components: {
+        NewMeal
+    }
+}
+</script>
